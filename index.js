5e43231eb0e956cbeea60d58c0f9355fc8458a4e
IAS\Ian.Nkwocha <ian.nkwocha@iris.co.uk>
2021-11-12 10:43:14 +0000

    Completed exercise 3

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log("Welcome to the Calculator");
+const readline = require('readline-sync');
+console.log('Welcome to the calculator\n===================');
+
+
+console.log('\nPlease enter the operator:');
+const operator = readline.prompt();
+
+console.log('\nPlease enter the first number:');
+const firstArgument = readline.prompt();
+const firstNumber = +firstArgument;
+
+console.log('\nPlease enter the second number:');
+const secondArgument = readline.prompt();
+const secondNumber = +secondArgument;
+
+let answer = 0;
+if (operator === '+') {
+    answer = firstNumber + secondNumber;
+} else if (operator === '-') {
+    answer = firstNumber - secondNumber;
+} else if (operator === '*') {
+    answer = firstNumber * secondNumber;
+} else if (operator === '/') {
+    answer = firstNumber / secondNumber;
+}
+
+console.log('\nThe answer is ' + answer);
